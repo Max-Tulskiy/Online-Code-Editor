2e1d7c14e900dbfd73c83755452ed59c5ed3fd0d
Max Tulskiy <max.tulskiy.04@gmail.com>
2023-12-28 02:38:56 +0300

    added connection with client

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI() 
+
+
+origins = [
+    'http://localhost:3000'
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+class Item(BaseModel):
+    code: str
+
+
+@app.post('/')
+async def input(data: Item):
+    print(data)
